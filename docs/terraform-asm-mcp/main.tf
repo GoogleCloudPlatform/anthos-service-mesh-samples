9f0744dc1cf1e4c84c64d398273c3b3a4bb94d28
Christine Kim <35876108+ckim328@users.noreply.github.com>
2022-05-03 14:44:45 -0400

    Terraform ASM with MCP setup (#51)
    * Add to gitignore
    
    * Adding readme setup
    
    * Added some output, started cleanup section
    
    * Added tfvars file
    
    * Added more to setup
    
    * Added Readme
    
    * Add more to README
    
    * gitignore, added variables.tf
    
    * Add more to readme and prereq
    
    * Corrected versions
    
    * Versions
    
    * Removed redundant folder, regional cluster
    
    * updated gitignore
    
    * Removed variables file
    
    * Updated readme
    
    * Remove empty folder
    
    * Split gke and asm tf
    
    * Fix license
    
    * Rename folder, address commits
    
    * Addressed nit comments
    
    * Addressed comments, removed localexec
    
    * Changed output
    
    * Renamed folder
    
    * Remove config_connector and private nodes setting
    
    * Folder structure
    
    * asm channel casing
    
    * Renamed folder, removed unecessary parts for single cluster setup
    
    * Edit in gcloud command
    
    * Removed criteria for depends on
    
    * Change default for variables
    
    * Removed depends on for apis
    
    * Removed ipranges

---
+++ docs/terraform-asm-mcp/main.tf
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+resource "google_gke_hub_membership" "membership" {
+  provider = google-beta
+
+  membership_id = "membership-${module.gke.name}"
+  endpoint {
+    gke_cluster {
+      resource_link = "//container.googleapis.com/${module.gke.cluster_id}"
+    }
+  }
+  depends_on = [module.gke.name, module.enable_google_apis]
+}
+
+module "asm" {
+  source              = "terraform-google-modules/kubernetes-engine/google//modules/asm"
+  version             = "~> 20.0"
+  project_id          = module.enable_google_apis.project_id
+  cluster_name        = module.gke.name
+  cluster_location    = var.region
+  channel             = var.asm_channel
+  enable_cni          = true
+  enable_mesh_feature = true
+}
