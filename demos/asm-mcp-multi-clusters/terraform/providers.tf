ac148e97cf3514c8e0248ba521cc3be1eab15fda
Mathieu Benoit <mabenoit@google.com>
2022-06-13 14:30:07 -0400

    Update providers.tf

--- demos/asm-mcp-multi-clusters/terraform/providers.tf
+++ demos/asm-mcp-multi-clusters/terraform/providers.tf
@@ -4,20 +4,9 @@ terraform {
       source  = "hashicorp/google"
       version = "= 4.24.0"
     }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "= 4.24.0"
-    }
   }
 }
 provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
-  zone    = var.gcp_zone
-}
-
-provider "google-beta" {
   project = var.gcp_project_id
   region  = var.gcp_region
   zone    = var.gcp_zone
