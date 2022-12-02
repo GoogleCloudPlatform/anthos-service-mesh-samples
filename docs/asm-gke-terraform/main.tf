f147a5599eeaf440be52c7418209860ac548e67f
ckim328 <christineskim@google.com>
2022-12-02 12:28:21 -0500

    Dependency resource

--- docs/asm-gke-terraform/main.tf
+++ docs/asm-gke-terraform/main.tf
@@ -1,13 +1,13 @@
 resource "google_container_cluster" "cluster" {
-  name                    = "my-cluster"
-  location                = var.zone
-  initial_node_count      = 1
-  provider                = google-beta
-  resource_labels = { mesh_id : "proj-${data.google_project.project.number}" }
+  name               = "my-cluster"
+  location           = var.zone
+  initial_node_count = 1
+  provider           = google-beta
+  resource_labels    = { mesh_id : "proj-${data.google_project.project.number}" }
   workload_identity_config {
     workload_pool = "${data.google_project.project.project_id}.svc.id.goog"
   }
-    depends_on = [
+  depends_on = [
     google_project_service.project
   ]
 }
@@ -29,10 +29,13 @@ resource "google_gke_hub_feature" "feature" {
   location = "global"
 
   provider = google-beta
+  depends_on = [
+    google_project_service.project
+  ]
 }
 
 resource "google_project_service" "project" {
-  project                  = var.project_id
+  project = var.project_id
   service = "mesh.googleapis.com"
 
   disable_dependent_services = true
